1a8c777c3e3634a6cb18d807e94800df5c554f27
Eddev92 <edwarllanca@hotmail.com>
2019-07-10 12:10:52 -0500

    apollo server up

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { ApolloServer, gql } = require('apollo-server');
+// const { ApolloServer, gql } = require('apollo-server');
 const express = require('express');
+var cors = require('cors')
 const graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var { buildSchema, GraphQLSchema } = require('graphql');
 
 const app = express();
-
+app.use(cors())
 
 // This is a (sample) collection of comments we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
@@ -13,7 +14,7 @@ const comments = [];
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
-const typeDefs = gql`
+const schema = buildSchema(`
   # Comments in GraphQL are defined with the hash (#) symbol.
   input CommentInput {
     comment: String
@@ -31,10 +32,10 @@ const typeDefs = gql`
    deleteSimpleComment(comment: String, position: Int): [Comment]
    getListComments: [Comment]
   }
-`;
+`);
 
 function addComment(args, comment) {
-  console.log(comment, 'comentario entrante')
+  console.log(comment.response, 'comentario entrante')
   comments.push(comment);
   return comments;
 }
@@ -54,41 +55,36 @@ function getComments() {
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve comments from the  comments" array above.
-const resolvers = {
-  Query: {
-   comments: () => comments,
+// const resolvers = {
+//   Query: {
+//    comments: () => comments,
+//    addSimpleComment: async (args,comment) => addComment(args, comment),
+//    deleteSimpleComment: async (args, comment) => deleteComment(args, comment),
+//    getListComments: async () => getComments
+//   },
+// };
+
+var root = {
+  comments: () => comments,
    addSimpleComment: async (args,comment) => addComment(args, comment),
    deleteSimpleComment: async (args, comment) => deleteComment(args, comment),
    getListComments: async () => getComments
-  },
-};
+}
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
-var schemaGetListComments = buildSchema(`
-# Comments in GraphQL are defined with the hash (#) symbol.
-input CommentInput {
-  comment: String
-}
-# This "Comment" type can be used in other type declarations.
-type Comment {
-  comment: String
-  position: Int
-}
-# The "Query" type is the root of all GraphQL queries.
-# (A "Mutation" type will be covered later on.)
-type Query {
- comments: [Comment]
- addSimpleComment(comment: String): Comment
- deleteSimpleComment(comment: String, position: Int): [Comment]
- getListComments: [Comment]
-}
-`);
+// const server = new ApolloServer({ typeDefs, root });
 
+// const schemaAux = new GraphQLSchema({typeDefs, root});
 app.use('/graphql', graphqlHTTP({
-  schema: schemaGetListComments,
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.post('/graphql', graphqlHTTP({
+  schema: schema,
   graphiql: true
 }));
 
